137f1eaf1206061fd4662d5aaec444254785622e
calv8n <ccalvin731@gmail.com>
2022-09-16 23:13:20 +0530

    Trying Initial

--- codewithstein/urls.py
+++ codewithstein/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 
 from blog.views import frontpage, post_detail
 
-
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
     path('<slug:slug>/', post_detail, name='post_detail'),
-    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
